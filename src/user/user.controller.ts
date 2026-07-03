cd070cc953157e5c46f3b2ac07e06c0c298b7816
Tina <tina@Tinas-MacBook-Pro.local>
2026-07-03 04:31:56 -0700

    fix(user):make srud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() user: CreateUserDto): Promise<User> {
+    return this.userService.createUser(user);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
@@ -10,12 +12,23 @@ export class UserService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findAll(): Promise<User[]> {
-    return this.userRepository.find();
-  }
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
 
-  async findOne(id: string): Promise<User | null> {
-    return this.userRepository.findOne({ where: { id } });
-  }
-  
+    async findOne(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+        where: { id },
+    });
+
+    if (!user) {
+        throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return user;
+    }
+
+    async createUser(user: CreateUserDto): Promise<User> {
+        return this.userRepository.save(user);
+    }
 }
